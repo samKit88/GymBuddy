e7f2d2a7ea57e89a2664ee61fde345da624c6f4d
samKit88 <samuel.girma.cx9896@gmail.com>
2023-09-03 02:59:19 -0800

    feat:Check the password and email

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../model/userModel');
+const User = require('../model/userModel.js');
 
 //user login
 const userLogin = async (req, res) => {
@@ -7,7 +7,15 @@ const userLogin = async (req, res) => {
 
 //user signup
 const userSignup = async (req, res) => {
-    res.status(200).json({message: 'signup'});
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.signup(email, password);
+
+        res.status(200).json({email, user});
+    }catch(error){
+        res.status(400).json({error: error.message});
+    };
 };
 
 module.exports = {
--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +15,37 @@ const userSchema = new Schema ({
         require: true,
     }
 });
+
+// static signup method
+userSchema.statics.signup = async function (email, password) {
+
+    // validation
+    if(!email || !password){
+        throw Error('All fileds must be filed');
+    };
+
+    if(!validator.isEmail(email)){
+        throw Error('Email is not valid');
+    }
+
+    if(!validator.isStrongPassword(password)){
+        throw Error('Password is not valid');
+    }
+    
+    //check the database 
+    const exist = await this.findOne({ email });
+
+    if(exist) {
+        throw Error('Email already in use');
+    };
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await this.create({email, password: hash});
+
+    return user
+};
+
 
 module.exports = mongoose.model('User', userSchema);
